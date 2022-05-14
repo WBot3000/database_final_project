bfeab1e73890b469e2c9c9b95f00bd5dedc084c0
WBot3000 <wvenusaur@gmail.com>
2022-05-14 11:12:36 -0400

    Added module, changed query 4 to include gv 0

--- final.py
+++ final.py
@@ -1,6 +1,6 @@
 import io
 
-#Functions used within this program
+#Converts an aggregate string (ex. sum_1_quant), into a list with 3 parts (ex. [sum, 1, quant])
 def agg_val_to_list(agg_value):
     list = ["", "", ""]
     index = 0
@@ -13,43 +13,46 @@ def agg_val_to_list(agg_value):
 
 #Basic reading of lines in the text file
 with open("./input.txt", 'r') as inputFile:
-    # Select Attributes will be stored in S as a list of strings
+    # Select Attributes will be stored in select_attrs as a list of strings
     # Commas will separate them, spaces and newlines will be removed
     select_attrs = inputFile.readline()
     select_attrs = select_attrs.replace(' ', '')
     select_attrs = select_attrs.replace('\n', '')
     select_attrs = select_attrs.split(',')
 
-    # Number of grouping variables will be stored in n as an int
+    # Number of grouping variables will be stored in num_grouping_vars as an int
     num_grouping_vars = inputFile.readline()
     num_grouping_vars = int(num_grouping_vars)
 
-    # Grouping Variables will be stored in V as a list of strings
+    # Grouping Variables will be stored in grouping_attrs as a list of strings
     # Commas will separate them, spaces and newlines will be removed
     grouping_attrs = inputFile.readline()
     grouping_attrs = grouping_attrs.replace(' ', '')
     grouping_attrs = grouping_attrs.replace('\n', '')
     grouping_attrs = grouping_attrs.split(',')
 
-    # F-Vects will be stored in F as a list of strings
+    # F-Vects will be stored in aggregate_vector as a list of strings
     # Commas will separate them, spaces and newlines will be removed
     # When these need to be converted into arrays, can iterate through list
+
+    #FTemp is a 1-D array of all the aggregates
     FTemp = inputFile.readline()
     FTemp = FTemp.replace(' ', '')
     FTemp = FTemp.replace('\n', '')
     FTemp = FTemp.split(',')
 
+    #aggregate_vector is a 2-D array, index i corresponds to gv i
     aggregate_vector = []
     for i in range(num_grouping_vars+1): #Needs to be +1 to account for gv 0
         aggregate_vector.append([])
     for agg in FTemp:
         agg_type, gv, agg_attribute = agg_val_to_list(agg)
-        gv = int(gv) #Turn gv into integer to be used as index
+        #Turn gv into integer to be used as index
+        gv = int(gv)
         aggregate_vector[gv].append(agg)
         
-    # Select Condition Vects will be stored in th as a list of strings
+    # Select Condition Vects will be stored in select_condition_vector as a list of strings
     # Commas will separate them, spaces and newlines will be removed
-    # Within these conditions, AND and OR statements are replaced with @@ or ||, respectively
     select_condition_vector = inputFile.readline()
     select_condition_vector = select_condition_vector.replace(' and ', '@@')
     select_condition_vector = select_condition_vector.replace(' or ', '||')
@@ -58,29 +61,22 @@ with open("./input.txt", 'r') as inputFile:
     select_condition_vector = select_condition_vector.replace('||', ' or ')
     select_condition_vector = select_condition_vector.replace('\n', '')
     select_condition_vector = select_condition_vector.split(',')
-    select_condition_vector = ["True"] + select_condition_vector #Selection condition for g.v. 0
+    #Selection condition for g.v. 0
+    select_condition_vector = ["True"] + select_condition_vector
 
     # Having Conditions will be stored in G as a string
-    # Newlines will be removed
-    # Within these conditions, AND and OR statements are replaced with @@ or ||, respectively
+    # Newline will be removed
     having_condition = inputFile.readline()
-    #having_condition = having_condition.replace(' and ', '@@')
-    #having_condition = having_condition.replace(' or ', '||')
-    #having_condition = having_condition.replace(' ', '')
-    #having_condition = having_condition.replace('@@', ' and ')
-    #having_condition = having_condition.replace('||', ' or ')
     having_condition = having_condition.replace('\n', '')
-    #having_condition = having_condition.split(',')
 
 #Write to solution file
 with open("./solution.py", 'w') as code:
 
-    #Function for getting grouping tuple
+    #Function for getting grouping tuple based on grouping_attrs
     def get_grouping_tuple():
         code.writelines("groupTuple = (")
         for i in range(len(grouping_attrs)):
             code.writelines("record[\"" + grouping_attrs[i] + "\"]")
-            #if i != len(grouping_attrs) - 1:
             code.writelines(", ")
         code.writelines(")\n")
 
@@ -93,6 +89,8 @@ with open("./solution.py", 'w') as code:
         return -1
     
     #Imports needed for the solution
+    #psycopg2 is the module that the solution needs to communicate with the database
+    #psycopg2.extras is the module that lets us reference database fields by name in the solution
     code.writelines("import psycopg2\nimport psycopg2.extras\n\n")
 
     #Writing the aggregate structure
@@ -101,13 +99,13 @@ with open("./solution.py", 'w') as code:
     for agg in FTemp: #Uses FTemp for convenience since it's one dimensional
         code.writelines("\t\tself." + agg + " = None\n")
         
-
+    #Establish connection with the database and fetch the records for the first time
     code.writelines("\noutput = {}\n\n")
     code.writelines("dbConnection = psycopg2.connect(dbname=\"postgres\", user=\"postgres\", password=\"password\", host=\"localhost\")\n")
     code.writelines("dbCursor = dbConnection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)\n") #cursor_factory = psycopg2.extras.RealDictCursor allows you to get fields using field name as opposed to index number
     code.writelines("dbCursor.execute(\"SELECT * FROM sales\")\n\n") #dbCursor now contains all the data from the table
 
-    #First loop
+    #First loop that gets all of the groups
     code.writelines("for record in dbCursor:\n")
     code.writelines("\t")
     get_grouping_tuple()
@@ -130,37 +128,28 @@ with open("./solution.py", 'w') as code:
             for agg in aggregates:
                 agg_type, gv, agg_attribute = agg_val_to_list(agg)
                 code.writelines("\t\tif group." + agg + " == None:\n")
+                #Different code for different aggregate functions
                 if agg_type == "count":
-                    #code.writelines("\t\t'Count Code'\n")
                     code.writelines("\t\t\tgroup." + agg + " = 1\n") #If value hasn't been set properly yet
                     code.writelines("\t\telse:\n")
                     code.writelines("\t\t\tgroup." + agg + " += 1\n") #Add +1 to count if it has been set
-                    #TODO: Count code goes here
                 elif agg_type == "sum":
-                    #code.writelines("\t\t'Sum Code'\n")
                     code.writelines("\t\t\tgroup." + agg + " = record[\"" + agg_attribute + "\"]\n") #If value hasn't been set properly yet
                     code.writelines("\t\telse:\n")
                     code.writelines("\t\t\tgroup." + agg + " += record[\"" + agg_attribute + "\"]\n") #Add +1 to count if it has been set
-                    #TODO
                 elif agg_type == "max":
-                    #code.writelines("\t\t'Max Code'\n")
                     code.writelines("\t\t\tgroup." + agg + " = record[\"" + agg_attribute + "\"]\n")
                     code.writelines("\t\telse:\n")
                     code.writelines("\t\t\tgroup." + agg + " = min(group." + agg + ", record[\"" + agg_attribute + "\"])\n")
-                    #TODO
                 elif agg_type == "min":
-                    #code.writelines("\t\t'Min Code'\n")
                     code.writelines("\t\t\tgroup." + agg + " = record[\"" + agg_attribute + "\"]\n")
                     code.writelines("\t\telse:\n")
                     code.writelines("\t\t\tgroup." + agg + " = min(group." + agg + ", record[\"" + agg_attribute + "\"])\n")
-                    #TODO
                 elif agg_type == "avg": #Average is stored as two index array, first index is sum, second is count. Average is then computed by dividing during printing
-                    #code.writelines("\t\t'Avg Code'\n")
                     code.writelines("\t\t\tgroup." + agg + " = [record[\"" + agg_attribute + "\"], 1]\n")
                     code.writelines("\t\telse:\n")
                     code.writelines("\t\t\tgroup." + agg + "[0] += record[\"" + agg_attribute + "\"]\n")
                     code.writelines("\t\t\tgroup." + agg + "[1] += 1\n")
-                    #TODO
             code.writelines("\n\n")
                 
     
@@ -168,7 +157,6 @@ with open("./solution.py", 'w') as code:
 
 
 #Having loop, where the output will be filtered
-#TODO
     code.writelines("for group_attrs, aggs in output.items():\n")
     code.writelines("\tif(" + having_condition + "):\n")
     code.writelines("\t\tprint('--------------------')\n")
--- solution.py
+++ solution.py
@@ -6,7 +6,6 @@ class AggStore:
 		self.avg_1_quant = None
 		self.avg_2_quant = None
 		self.avg_3_quant = None
-		self.avg_4_quant = None
 
 output = {}
 
@@ -15,15 +14,15 @@ dbCursor = dbConnection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 dbCursor.execute("SELECT * FROM sales")
 
 for record in dbCursor:
-	groupTuple = (record["cust"], record["prod"], )
+	groupTuple = (record["cust"], )
 	testIfThere = output.get(groupTuple)
 	if testIfThere == None:
 		output[groupTuple] = AggStore()
 
 dbCursor.execute("SELECT * FROM sales")
 for record in dbCursor:
-	if (record["month"]>=1 and record["month"]<=3):
-		groupTuple = (record["cust"], record["prod"], )
+	if (record["state"]=='NY'):
+		groupTuple = (record["cust"], )
 		group = output.get(groupTuple)
 		if group.avg_1_quant == None:
 			group.avg_1_quant = [record["quant"], 1]
@@ -34,8 +33,8 @@ for record in dbCursor:
 
 dbCursor.execute("SELECT * FROM sales")
 for record in dbCursor:
-	if (record["month"]>=4 and record["month"]<=6):
-		groupTuple = (record["cust"], record["prod"], )
+	if (record["state"]=='NJ'):
+		groupTuple = (record["cust"], )
 		group = output.get(groupTuple)
 		if group.avg_2_quant == None:
 			group.avg_2_quant = [record["quant"], 1]
@@ -46,8 +45,8 @@ for record in dbCursor:
 
 dbCursor.execute("SELECT * FROM sales")
 for record in dbCursor:
-	if (record["month"]>=7 and record["month"]<=9):
-		groupTuple = (record["cust"], record["prod"], )
+	if (record["state"]=='CT'):
+		groupTuple = (record["cust"], )
 		group = output.get(groupTuple)
 		if group.avg_3_quant == None:
 			group.avg_3_quant = [record["quant"], 1]
@@ -56,25 +55,11 @@ for record in dbCursor:
 			group.avg_3_quant[1] += 1
 
 
-dbCursor.execute("SELECT * FROM sales")
-for record in dbCursor:
-	if (record["month"]>=10 and record["month"]<=12):
-		groupTuple = (record["cust"], record["prod"], )
-		group = output.get(groupTuple)
-		if group.avg_4_quant == None:
-			group.avg_4_quant = [record["quant"], 1]
-		else:
-			group.avg_4_quant[0] += record["quant"]
-			group.avg_4_quant[1] += 1
-
-
 for group_attrs, aggs in output.items():
-	if(True):
+	if(aggs.avg_1_quant[0]/aggs.avg_1_quant[1] > aggs.avg_2_quant[0]/aggs.avg_2_quant[1] and aggs.avg_1_quant[0]/aggs.avg_1_quant[1] > aggs.avg_3_quant[0]/aggs.avg_3_quant[1]):
 		print('--------------------')
 		print('cust: ', group_attrs[0])
-		print('prod: ', group_attrs[1])
 		print('avg_1_quant: ', aggs.avg_1_quant[0]/aggs.avg_1_quant[1])
 		print('avg_2_quant: ', aggs.avg_2_quant[0]/aggs.avg_2_quant[1])
 		print('avg_3_quant: ', aggs.avg_3_quant[0]/aggs.avg_3_quant[1])
-		print('avg_4_quant: ', aggs.avg_4_quant[0]/aggs.avg_4_quant[1])
 		print('--------------------')
